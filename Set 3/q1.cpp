3107ff7648185ce394953040768bc66affe41c84
sandeepgiri-git <sandeep0786giri@gmail.com>
2024-11-28 22:17:38 +0530

    Changed

--- Set 3/q1.cpp
+++ Set 3/q1.cpp
@@ -1,16 +1,6 @@
 #include<iostream>
 using namespace std;
 
-void print(int a[],int n){
-    for (int i = 0; i < n; i++)
-    {
-        cout<<a[i]<<" ";
-    }
-    cout<<endl;
-    
-}
-//pushed
-
 bool checkPerfectSquare(int n){
     for(int i = 0;i<n/2;i++){
         if(i*i == n){
